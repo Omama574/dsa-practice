4e18773eaad0f89cd631ee558099239da4286b9e
Omama574 <178472699+Omama574@users.noreply.github.com>
2025-11-18 09:21:45 +0530

    Added Valid Anagram solution using hash map

---
+++ strings/valid-anagram.js
@@ -0,0 +1,22 @@
+
+
+function isAnagram(s, t) {
+    if (s.length !== t.length) return false;
+
+    const count = {};
+
+    for (let char of s) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    for (let char of t) {
+        if (!count[char]) return false;
+        count[char]--;
+    }
+
+    return true;
+}
+
+
+console.log(isAnagram("listen", "silent"));
+console.log(isAnagram("rat", "car"));       
